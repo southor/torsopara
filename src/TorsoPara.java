6b147b8e7ff1b59c75cd0fffa2dc47735e014a8a
t.soderberg8 <t.soderberg8@47a34bec-3f04-11de-be05-5f7a86268029>
2009-05-15 15:16:17 +0000

    ...

--- src/TorsoPara.java
+++ src/TorsoPara.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Chuffo
@@ -6,5 +9,9 @@
  * To change this template use File | Settings | File Templates.
  */
 public class TorsoPara {
-    public static void main() {}
+    public static void main() {
+        LinkedBlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>();
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(4, 4, 500, java.util.concurrent.TimeUnit.MILLISECONDS, workQueue);
+    }
 }
+
