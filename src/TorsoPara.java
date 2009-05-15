d1c3bab493dfb527f91625635b913131ade45c39
t.soderberg8 <t.soderberg8@47a34bec-3f04-11de-be05-5f7a86268029>
2009-05-15 12:49:19 +0000

    starting adding files

---
+++ src/TorsoPara.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chuffo
+ * Date: 2009-maj-15
+ * Time: 14:46:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class TorsoPara {
+    public static void main() {}
+}
